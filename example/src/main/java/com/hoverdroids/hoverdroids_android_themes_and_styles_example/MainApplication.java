7d3ddc611770761dc700a767b507748b32192b80
Chris Sprague <spragucm@gmail.com>
2018-11-14 17:06:56 +0100

    Refactored all of the package names to avoid naming conflicts with libraries and other projects. All of the themes from AndroidMaterialTheme Demo are implemented using libraries instead of project specific resources. Fixed bug that prevented seekbar and progressbar styling from working on some devices-e.g. Samsung Galaxy running 5.0.1

---
+++ example/src/main/java/com/hoverdroids/hoverdroids_android_themes_and_styles_example/MainApplication.java
@@ -0,0 +1,20 @@
+package com.hoverdroids.hoverdroids_android_themes_and_styles_example;
+
+import android.app.Application;
+import com.hoverdroids.logutils.LogUtils;
+import timber.log.Timber;
+
+/**
+ * @author Chris Sprague
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize logging first to log all operations
+        LogUtils.initLoggingUtilities();
+        Timber.v(LogUtils.METHOD_ONLY);
+    }
+}
